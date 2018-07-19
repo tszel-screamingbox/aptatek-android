88a26486237d1d0c540a7d2eb194dafc429f2f6b
Renes <renespeter@hotmail.com>
2018-07-19 23:47:39 +0200

    weekly chart screen

--- app/src/main/java/com/aptatek/aptatek/view/weekly/WeeklyResultActivity.java
+++ app/src/main/java/com/aptatek/aptatek/view/weekly/WeeklyResultActivity.java
@@ -2,6 +2,8 @@ package com.aptatek.aptatek.view.weekly;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.Group;
+import android.view.View;
 
 import com.aptatek.aptatek.R;
 import com.aptatek.aptatek.injection.component.ActivityComponent;
@@ -9,17 +11,22 @@ import com.aptatek.aptatek.view.base.BaseActivity;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class WeeklyResultActivity extends BaseActivity<WeeklyResultActivityView, WeeklyResultActivityPresenter> implements WeeklyResultActivityView {
 
     @Inject
     WeeklyResultActivityPresenter presenter;
 
+    @BindView(R.id.emptyGroup)
+    Group emptyGroup;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_weekly);
         ButterKnife.bind(this);
 
     }
@@ -40,5 +47,8 @@ public class WeeklyResultActivity extends BaseActivity<WeeklyResultActivityView,
         return 0;
     }
 
-
+    @OnClick(R.id.playIcon)
+    public void onPlayButtonClicked() {
+        emptyGroup.setVisibility(View.GONE);
+    }
 }
