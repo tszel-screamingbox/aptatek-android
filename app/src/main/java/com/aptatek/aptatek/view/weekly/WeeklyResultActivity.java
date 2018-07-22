3cb9b4682e9708075d8a985825f34814f7a9a3dc
Renes <renespeter@hotmail.com>
2018-07-23 00:28:10 +0200

    weekly chart screen

--- app/src/main/java/com/aptatek/aptatek/view/weekly/WeeklyResultActivity.java
+++ app/src/main/java/com/aptatek/aptatek/view/weekly/WeeklyResultActivity.java
@@ -1,5 +1,7 @@
 package com.aptatek.aptatek.view.weekly;
 
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.Group;
@@ -9,6 +11,7 @@ import com.aptatek.aptatek.R;
 import com.aptatek.aptatek.injection.component.ActivityComponent;
 import com.aptatek.aptatek.view.base.BaseActivity;
 import com.github.mikephil.charting.charts.BubbleChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BubbleData;
@@ -26,6 +29,10 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class WeeklyResultActivity extends BaseActivity<WeeklyResultActivityView, WeeklyResultActivityPresenter> implements WeeklyResultActivityView {
+
+    private static final float MIN_X = -0.5f;
+    private static final float MAX_X = 6.5f;
+    private static final float OFFSET = 2f;
 
     @Inject
     WeeklyResultActivityPresenter presenter;
@@ -63,32 +70,58 @@ public class WeeklyResultActivity extends BaseActivity<WeeklyResultActivityView,
     @OnClick(R.id.playIcon)
     public void onPlayButtonClicked() {
         emptyGroup.setVisibility(View.GONE);
+
+        addEntry();
+    }
+
+    private void addEntry() {
+        final BubbleData data = weeklyBubbleChart.getBubbleData();
+
+
+        final ArrayList<BubbleEntry> entries = new ArrayList<>();
+        entries.add(new BubbleEntry(0, 0, 0.1f, "test"));
+        entries.add(new BubbleEntry(0, 13, 0.1f, "10"));
+        entries.add(new BubbleEntry(1, 7, 0.1f, "50"));
+        entries.add(new BubbleEntry(1, 10, 0.1f, "30"));
+        entries.add(new BubbleEntry(2, 8, 0.1f, "10"));
+        entries.add(new BubbleEntry(2, 12, 0.1f, "10"));
+        entries.add(new BubbleEntry(3, 20, 0.1f, "20"));
+        entries.add(new BubbleEntry(4, 23, 0.1f, "15"));
+//        entries.add(new BubbleEntry(5, 22, 0.1f));
+        entries.add(new BubbleEntry(6, 23, 0.1f));
+        entries.add(new BubbleEntry(6, 11, 0.1f));
+
+        final BubbleDataSet dataSet = new BubbleDataSet(entries, null);
+        dataSet.setColors(Color.RED, Color.GRAY, Color.GREEN);
+
+        data.addDataSet(dataSet);
+        data.notifyDataChanged();
+        weeklyBubbleChart.invalidate();
     }
 
     private void initChart() {
-        final ArrayList<BubbleEntry> entries = new ArrayList<>();
-        entries.add(new BubbleEntry(0, 5, 0.2f));
-        entries.add(new BubbleEntry(0, 13, 0.2f));
-        entries.add(new BubbleEntry(1, 6, 0.2f));
-        entries.add(new BubbleEntry(1, 10, 0.2f));
-        entries.add(new BubbleEntry(2, 4, 0.2f));
-        entries.add(new BubbleEntry(2, 12, 0.2f));
-        entries.add(new BubbleEntry(3, 20, 0.2f));
-        entries.add(new BubbleEntry(4, 22, 0.2f));
-        entries.add(new BubbleEntry(5, 22, 0.2f));
-        entries.add(new BubbleEntry(6, 23, 0.2f));
-
-        final BubbleDataSet dataSet = new BubbleDataSet(entries, "");
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
         final XAxis xAxis = weeklyBubbleChart.getXAxis();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(getResources().getStringArray(R.array.weekly_days)));
+        final String[] days = getResources().getStringArray(R.array.weekly_days);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(days));
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisMinimum(MIN_X);
+        xAxis.setAxisMaximum(MAX_X);
         final YAxis yAxis = weeklyBubbleChart.getAxisLeft();
         final String[] hours = getResources().getStringArray(R.array.weekly_hours);
         yAxis.setValueFormatter(new IndexAxisValueFormatter(hours));
-        yAxis.setGranularity(1f); // interval 1
         yAxis.setLabelCount(hours.length);
-        weeklyBubbleChart.getAxisLeft().setInverted(true);
-        final BubbleData data = new BubbleData(dataSet);
+        yAxis.setDrawAxisLine(false);
+        yAxis.setDrawGridLines(false);
+        yAxis.setInverted(true);
+        yAxis.setAxisMinimum(-OFFSET);
+        yAxis.setAxisMaximum(hours.length + OFFSET);
+        weeklyBubbleChart.getAxisRight().setEnabled(false);
+        weeklyBubbleChart.getAxisRight().setDrawGridLines(false);
+        weeklyBubbleChart.setDrawBorders(false);
+        weeklyBubbleChart.setScaleEnabled(false);
+        weeklyBubbleChart.getDescription().setEnabled(false);
+        final BubbleData data = new BubbleData();
         weeklyBubbleChart.setData(data);
     }
 }
