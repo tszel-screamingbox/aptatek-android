c16f1babe39503a8cc09aabddd5eb38126094187
Tamas Szelezsan <tamas.szelezsan@screamingbox.com>
2018-08-07 14:11:05 +0200

    Generate random data with timestamps at least 40 mins between each other

--- app/src/main/java/com/aptatek/pkuapp/data/datasource/FakeCubeDataGenerator.java
+++ app/src/main/java/com/aptatek/pkuapp/data/datasource/FakeCubeDataGenerator.java
@@ -13,10 +13,13 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import ix.Ix;
+
 @SuppressWarnings("magicNumber")
 public class FakeCubeDataGenerator {
 
     private static final long DAY_IN_MILLIS = 1000L * 60 * 60 * 24;
+    private static final long MIN_TIME_BETWEEN_MEASURES = 1000L * 60 * 40;
     private static final int MAX_MEASUREMENTS_PER_DAY = 5;
     private static final int FIRST_DATA_BEFORE_TODAY_IN_DAYS = 7 * 3 - 2;
 
@@ -35,7 +38,10 @@ public class FakeCubeDataGenerator {
         for (int day = 0; day <= daysBetween; day++) {
             final int numOfMeasurementsThatDay = dataFactory.getNumberUpTo(MAX_MEASUREMENTS_PER_DAY);
             for (int measurement = 0; measurement < numOfMeasurementsThatDay; measurement++) {
-                final CubeDataModel cubeDataModel = generateDataForGivenDay(TimeHelper.addDays(day, begin), false);
+                final CubeDataModel cubeDataModel = generateDataForGivenDay(TimeHelper.addDays(day, begin));
+
+                ensureCubeDataTimeBetweenRandomData(cubeDataModels, cubeDataModel);
+
                 cubeDataModels.add(cubeDataModel);
             }
         }
@@ -43,23 +49,43 @@ public class FakeCubeDataGenerator {
         return cubeDataModels;
     }
 
+    private void ensureCubeDataTimeBetweenRandomData(final List<CubeDataModel> randomModels, final CubeDataModel cubeDataModel) {
+        final boolean hasCollision = Ix.from(randomModels)
+            .filter(model -> Math.abs(cubeDataModel.getTimestamp() - model.getTimestamp()) < MIN_TIME_BETWEEN_MEASURES)
+            .count()
+            .single() > 0;
+        if (hasCollision) {
+            cubeDataModel.setTimestamp(generateRandomTimeAtGivenDay(cubeDataModel.getTimestamp()));
+        }
+    }
+
     @NonNull
-    public CubeDataModel generateDataForGivenDay(final long timestamp, boolean useExactTime) {
+    public CubeDataModel generateDataForGivenDay(final long timestamp) {
         final CubeDataModel cubeDataModel = new CubeDataModel();
         cubeDataModel.setCubeId(dataFactory.getRandomChars(10));
         cubeDataModel.setId(dataFactory.getNumberUpTo(Integer.MAX_VALUE));
         cubeDataModel.setValueInMMol(dataFactory.getNumberUpTo((int) Constants.DEFAULT_PKU_HIGHEST_VALUE));
-        final long earliestTimeAtGivenDay = TimeHelper.getEarliestTimeAtGivenDay(timestamp);
-        final long randomOffset = dataFactory.getNumberUpTo((int) DAY_IN_MILLIS);
-        cubeDataModel.setTimestamp(earliestTimeAtGivenDay + (useExactTime ? 0 : randomOffset));
+        cubeDataModel.setTimestamp(generateRandomTimeAtGivenDay(timestamp));
 
         return cubeDataModel;
     }
 
+    private long generateRandomTimeAtGivenDay(final long timestamp) {
+        final long earliestTimeAtGivenDay = TimeHelper.getEarliestTimeAtGivenDay(timestamp);
+        final long randomOffset = dataFactory.getNumberUpTo((int) DAY_IN_MILLIS);
+        return earliestTimeAtGivenDay + randomOffset;
+    }
+
     @NonNull
     public CubeDataModel generateOldestData() {
-        return generateDataForGivenDay(TimeHelper.addDays(FIRST_DATA_BEFORE_TODAY_IN_DAYS * -1,
-                TimeHelper.getEarliestTimeAtGivenDay(System.currentTimeMillis())), true);
+        final long earliestTimeAtGivenDay = TimeHelper.getEarliestTimeAtGivenDay(System.currentTimeMillis());
+        final long oldestTimestamp = TimeHelper.addDays(FIRST_DATA_BEFORE_TODAY_IN_DAYS * -1, earliestTimeAtGivenDay);
+        final CubeDataModel cubeDataModel = generateDataForGivenDay(
+                oldestTimestamp
+        );
+        cubeDataModel.setTimestamp(oldestTimestamp);
+
+        return cubeDataModel;
     }
 
 }
