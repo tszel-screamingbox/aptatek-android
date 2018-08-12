28d2ea7b0d9f39e6c29d5d5cadabc8664ba54723
Robert Banyai <>
2018-08-12 16:12:06 +0200

    Fix title at home screen.

--- app/src/main/java/com/aptatek/pkuapp/view/main/MainActivityPresenter.java
+++ app/src/main/java/com/aptatek/pkuapp/view/main/MainActivityPresenter.java
@@ -16,7 +16,6 @@ import com.aptatek.pkuapp.view.main.adapter.DailyResultAdapterItem;
 import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -49,38 +48,32 @@ class MainActivityPresenter extends MvpBasePresenter<MainActivityView> {
     // TODO should load data on demand, per weeks / pages... Getting the whole dataSet will have perf impacts
     void loadData() {
         disposables.add(
-            rangeInteractor.getInfo()
-                .flatMap(rangeInfo ->
-                    cubeInteractor.listAll()
-                        .map(list -> new Pair<>(rangeInfo, list))
-                )
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(pair -> {
-                    final List<ChartVM> chartVMS = ChartUtils.asChartVMList(pair.second, pair.first);
-                    chartVMS.set(chartVMS.size() - 1, chartVMS.get(chartVMS.size() - 1).toBuilder().setZoomed(true).build());
+                rangeInteractor.getInfo()
+                        .flatMap(rangeInfo ->
+                                cubeInteractor.listAll()
+                                        .map(list -> new Pair<>(rangeInfo, list))
+                        )
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(pair -> {
+                            final List<ChartVM> chartVMS = ChartUtils.asChartVMList(pair.second, pair.first);
+                            final ChartVM lastResult = chartVMS.get(chartVMS.size() - 1).toBuilder().setZoomed(true).build();
+                            chartVMS.set(chartVMS.size() - 1, lastResult);
 
-                    ifViewAttached(attachedView -> attachedView.displayData(chartVMS));
-                })
+                            ifViewAttached(attachedView -> {
+                                attachedView.updateTitles(
+                                        formatTitle(lastResult),
+                                        dailyChartFormatter.formatDate(lastResult.getDate().getTime(), lastResult.getNumberOfMeasures() > 0));
+                                attachedView.displayData(chartVMS);
+                            });
+                        })
         );
     }
 
     void itemZoomIn(final ChartVM chartVM) {
-        final Date date = chartVM.getDate();
-        final String subTitle = dailyChartFormatter.formatDate(date.getTime(), chartVM.getNumberOfMeasures() > 0);
-        final String title;
-
-        if (DateUtils.isToday(date.getTime())) {
-            title = resourceInteractor.getStringResource(R.string.main_title_today);
-        } else if (DateUtils.isToday(date.getTime() + DateUtils.DAY_IN_MILLIS)) {
-            title = resourceInteractor.getStringResource(R.string.main_title_yesterday);
-        } else {
-            final Calendar cal = Calendar.getInstance();
-            cal.setTime(date);
-            title = dailyChartFormatter.getNameOfDay(date.getTime());
-        }
-
         ifViewAttached(view -> {
-            view.updateTitles(title, subTitle);
+            view.updateTitles(
+                    formatTitle(chartVM),
+                    dailyChartFormatter.formatDate(chartVM.getDate().getTime(), chartVM.getNumberOfMeasures() > 0));
             view.changeItemZoomState(chartVM, chartVM.toBuilder().setZoomed(true).build());
         });
     }
@@ -91,26 +84,26 @@ class MainActivityPresenter extends MvpBasePresenter<MainActivityView> {
 
     void measureListToAdapterList(final List<CubeData> measures) {
         disposables.add(
-            rangeInteractor.getInfo()
-                .map(rangeInfo ->
-                    Ix.from(measures)
-                        .map(cubeData -> {
-                            final CharSequence details = dailyChartFormatter.getBubbleText(cubeData.getPkuLevel());
-                            final ChartUtils.State state = ChartUtils.getState(cubeData.getPkuLevel(), rangeInfo);
-                            return DailyResultAdapterItem.create(
-                                    details,
-                                    cubeData.getTimestamp(),
-                                    ChartUtils.smallBubbleBackground(state),
-                                    ChartUtils.stateColor(state));
-                        })
-                        .orderBy((o1, o2) -> Long.compare(o1.getTimestamp(), o2.getTimestamp()))
-                        .toList()
-                )
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(adapterItems ->
-                    ifViewAttached(view -> view.setMeasureList(adapterItems))
-                )
+                rangeInteractor.getInfo()
+                        .map(rangeInfo ->
+                                Ix.from(measures)
+                                        .map(cubeData -> {
+                                            final CharSequence details = dailyChartFormatter.getBubbleText(cubeData.getPkuLevel());
+                                            final ChartUtils.State state = ChartUtils.getState(cubeData.getPkuLevel(), rangeInfo);
+                                            return DailyResultAdapterItem.create(
+                                                    details,
+                                                    cubeData.getTimestamp(),
+                                                    ChartUtils.smallBubbleBackground(state),
+                                                    ChartUtils.stateColor(state));
+                                        })
+                                        .orderBy((o1, o2) -> Long.compare(o1.getTimestamp(), o2.getTimestamp()))
+                                        .toList()
+                        )
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(adapterItems ->
+                                ifViewAttached(view -> view.setMeasureList(adapterItems))
+                        )
         );
     }
 
@@ -128,5 +121,20 @@ class MainActivityPresenter extends MvpBasePresenter<MainActivityView> {
         }
 
         super.detachView();
+    }
+
+    private String formatTitle(final ChartVM chartVM) {
+        final String title;
+
+        if (DateUtils.isToday(chartVM.getDate().getTime())) {
+            title = resourceInteractor.getStringResource(R.string.main_title_today);
+        } else if (DateUtils.isToday(chartVM.getDate().getTime() + DateUtils.DAY_IN_MILLIS)) {
+            title = resourceInteractor.getStringResource(R.string.main_title_yesterday);
+        } else {
+            final Calendar cal = Calendar.getInstance();
+            cal.setTime(chartVM.getDate());
+            title = dailyChartFormatter.getNameOfDay(chartVM.getDate().getTime());
+        }
+        return title;
     }
 }
