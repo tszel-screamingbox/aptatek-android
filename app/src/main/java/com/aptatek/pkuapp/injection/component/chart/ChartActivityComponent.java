9dd2dba5e40ad015be8ce223d56a491823aa474b
Tamas Szelezsan <tamas.szelezsan@screamingbox.com>
2018-08-26 18:09:49 +0200

    Change text on fingerprint description of settings

--- app/src/main/java/com/aptatek/pkuapp/injection/component/chart/ChartActivityComponent.java
+++ app/src/main/java/com/aptatek/pkuapp/injection/component/chart/ChartActivityComponent.java
@@ -3,7 +3,6 @@ package com.aptatek.pkuapp.injection.component.chart;
 import com.aptatek.pkuapp.injection.module.ActivityModule;
 import com.aptatek.pkuapp.injection.module.chart.ChartModule;
 import com.aptatek.pkuapp.injection.module.rangeinfo.RangeInfoModule;
-import com.aptatek.pkuapp.view.main.MainActivity;
 import com.aptatek.pkuapp.view.weekly.WeeklyResultActivity;
 
 import dagger.Subcomponent;
