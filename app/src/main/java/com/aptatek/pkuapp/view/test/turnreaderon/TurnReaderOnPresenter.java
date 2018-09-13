a6ebe96071b426276769b2f8819b3967d34f13e7
Robert Banyai <>
2018-09-13 18:26:41 +0200

    Change Turn Reader On video and remove automatically next on Turn Reader On page. ( Request from Balazs )

--- app/src/main/java/com/aptatek/pkuapp/view/test/turnreaderon/TurnReaderOnPresenter.java
+++ app/src/main/java/com/aptatek/pkuapp/view/test/turnreaderon/TurnReaderOnPresenter.java
@@ -4,44 +4,13 @@ import com.aptatek.pkuapp.R;
 import com.aptatek.pkuapp.domain.interactor.ResourceInteractor;
 import com.aptatek.pkuapp.view.test.base.TestBasePresenter;
 
-import java.util.concurrent.TimeUnit;
-
 import javax.inject.Inject;
 
-import io.reactivex.Flowable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-
 public class TurnReaderOnPresenter extends TestBasePresenter<TurnReaderOnView> {
-
-    private static final long FAKE_CONNECT_DELAY = 5000L;
-
-    private CompositeDisposable compositeDisposable;
 
     @Inject
     public TurnReaderOnPresenter(final ResourceInteractor resourceInteractor) {
         super(resourceInteractor);
-    }
-
-    @Override
-    public void attachView(final TurnReaderOnView view) {
-        super.attachView(view);
-
-        compositeDisposable = new CompositeDisposable();
-
-        compositeDisposable.add(Flowable.timer(FAKE_CONNECT_DELAY, TimeUnit.MILLISECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(ignore -> ifViewAttached(TurnReaderOnView::onReaderConnected))
-        );
-    }
-
-    @Override
-    public void detachView() {
-        if (compositeDisposable != null && !compositeDisposable.isDisposed()) {
-            compositeDisposable.dispose();
-        }
-
-        super.detachView();
     }
 
     @Override
@@ -50,8 +19,6 @@ public class TurnReaderOnPresenter extends TestBasePresenter<TurnReaderOnView> {
             attachedView.setTitle(resourceInteractor.getStringResource(R.string.test_turnreaderon_title));
             attachedView.setMessage(resourceInteractor.getStringResource(R.string.test_turnreaderon_message));
             attachedView.playVideo(resourceInteractor.getUriForRawFile(R.raw.turn_reader_on), true);
-            attachedView.setBatteryIndicatorVisible(true);
-            attachedView.setBatteryPercentageText("100%");
         });
     }
 }
