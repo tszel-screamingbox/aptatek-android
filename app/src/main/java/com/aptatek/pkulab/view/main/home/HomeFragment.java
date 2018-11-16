30d228c7528e2e01ca8b480362afa3f2acf700d1
Robert Banyai <>
2018-11-16 18:35:41 +0100

    Fix pmd.

--- app/src/main/java/com/aptatek/pkulab/view/main/home/HomeFragment.java
+++ app/src/main/java/com/aptatek/pkulab/view/main/home/HomeFragment.java
@@ -2,7 +2,6 @@ package com.aptatek.pkulab.view.main.home;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
 import android.support.constraint.Group;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
