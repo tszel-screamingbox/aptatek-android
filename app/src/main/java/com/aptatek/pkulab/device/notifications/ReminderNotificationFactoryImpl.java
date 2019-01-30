3a084c78dddae6513904c921bc345b4f487aa253
Renes <renespeter@hotmail.com>
2019-01-30 21:34:24 +0100

    pmd fix

--- app/src/main/java/com/aptatek/pkulab/device/notifications/ReminderNotificationFactoryImpl.java
+++ app/src/main/java/com/aptatek/pkulab/device/notifications/ReminderNotificationFactoryImpl.java
@@ -1,19 +1,16 @@
 package com.aptatek.pkulab.device.notifications;
 
 import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 
 import com.aptatek.pkulab.R;
 import com.aptatek.pkulab.device.ReminderActionReceiver;
 import com.aptatek.pkulab.device.ReminderActionType;
-import com.aptatek.pkulab.device.notifications.BaseNotificationFactory;
 import com.aptatek.pkulab.domain.interactor.ResourceInteractor;
 import com.aptatek.pkulab.domain.interactor.remindersettings.ReminderNotificationFactory;
 import com.aptatek.pkulab.injection.qualifier.ApplicationContext;
