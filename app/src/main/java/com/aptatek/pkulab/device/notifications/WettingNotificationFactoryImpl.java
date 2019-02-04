1b9c0a5453420a365a7a32f81ec938a42ea18074
Tamas Szelezsan <tamas.szelezsan@screamingbox.com>
2019-02-04 09:29:51 +0100

    Wetting countdown should not play sound

--- app/src/main/java/com/aptatek/pkulab/device/notifications/WettingNotificationFactoryImpl.java
+++ app/src/main/java/com/aptatek/pkulab/device/notifications/WettingNotificationFactoryImpl.java
@@ -1,11 +1,13 @@
 package com.aptatek.pkulab.device.notifications;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 
@@ -17,6 +19,9 @@ import com.aptatek.pkulab.util.Constants;
 import com.aptatek.pkulab.view.test.TestActivity;
 
 public class WettingNotificationFactoryImpl extends BaseNotificationFactory implements WettingNotificationFactory {
+
+    private static final String COUNTDOWN_CHANNEL_ID = "pkulab.wetting";
+
     public WettingNotificationFactoryImpl(@ApplicationContext final Context context,
                                           final ResourceInteractor resourceInteractor,
                                           final NotificationManager notificationManager) {
@@ -32,10 +37,24 @@ public class WettingNotificationFactoryImpl extends BaseNotificationFactory impl
         return stackBuilder.getPendingIntent(1, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
+    private String createCountdownChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            final NotificationChannel notificationChannel = new NotificationChannel(
+                    COUNTDOWN_CHANNEL_ID,
+                    resourceInteractor.getStringResource(R.string.test_countdown_notification_channel_name),
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.enableVibration(false);
+            notificationChannel.setSound(null, null);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        return COUNTDOWN_CHANNEL_ID;
+    }
+
     @NonNull
     @Override
     public Notification createCountdownNotification(@NonNull final Countdown countdown) {
-        return new NotificationCompat.Builder(context, createChannel())
+        return new NotificationCompat.Builder(context, createCountdownChannel())
                 .setContentTitle(resourceInteractor.getStringResource(R.string.test_wetting_notification_inprogress_title))
                 .setContentText(resourceInteractor.getStringResource(R.string.test_wetting_notification_inprogress_textformat, countdown.getRemainingFormattedText()))
                 .setSmallIcon(R.drawable.ic_notification)
