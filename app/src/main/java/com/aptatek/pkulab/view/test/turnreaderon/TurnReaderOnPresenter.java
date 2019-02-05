ff15a958a53952a6e9a24873d073f3f7ebd5b1bd
Renes <renespeter@hotmail.com>
2019-02-05 22:47:23 +0100

    added battery view

--- app/src/main/java/com/aptatek/pkulab/view/test/turnreaderon/TurnReaderOnPresenter.java
+++ app/src/main/java/com/aptatek/pkulab/view/test/turnreaderon/TurnReaderOnPresenter.java
@@ -2,11 +2,19 @@ package com.aptatek.pkulab.view.test.turnreaderon;
 
 import com.aptatek.pkulab.R;
 import com.aptatek.pkulab.domain.interactor.ResourceInteractor;
+import com.aptatek.pkulab.domain.interactor.countdown.Countdown;
+import com.aptatek.pkulab.view.test.TestActivityCommonView;
 import com.aptatek.pkulab.view.test.base.TestBasePresenter;
 
 import javax.inject.Inject;
 
+import io.reactivex.disposables.Disposable;
+
 public class TurnReaderOnPresenter extends TestBasePresenter<TurnReaderOnView> {
+
+    private static final long MOCK_SELF_TEST_DURATION = 5000L;
+
+    private Disposable disposable;
 
     @Inject
     public TurnReaderOnPresenter(final ResourceInteractor resourceInteractor) {
@@ -19,6 +27,27 @@ public class TurnReaderOnPresenter extends TestBasePresenter<TurnReaderOnView> {
             attachedView.setTitle(resourceInteractor.getStringResource(R.string.test_turnreaderon_title));
             attachedView.setMessage(resourceInteractor.getStringResource(R.string.test_turnreaderon_message));
             attachedView.playVideo(resourceInteractor.getUriForRawFile(R.raw.turn_reader_on), true);
+            attachedView.setBatteryIndicatorVisible(true);
+            attachedView.setBatteryPercentage(100);
         });
+
+        disposeSubscription();
+        disposable = Countdown.countdown(MOCK_SELF_TEST_DURATION, (tick) -> tick >= MOCK_SELF_TEST_DURATION, (tick) -> tick)
+                .subscribe(ignored -> ifViewAttached(TestActivityCommonView::showNextScreen));
+
+    }
+
+    private void disposeSubscription() {
+        if (disposable != null) {
+            disposable.dispose();
+            disposable = null;
+        }
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+
+        disposeSubscription();
     }
 }
