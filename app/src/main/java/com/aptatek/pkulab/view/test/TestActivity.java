764894f7675801d0d27f89438500e4444b0b2081
Tamas Szelezsan <tamas.szelezsan@screamingbox.com>
2019-04-27 15:17:47 +0200

    Remove screen indicator dot for Testing

--- app/src/main/java/com/aptatek/pkulab/view/test/TestActivity.java
+++ app/src/main/java/com/aptatek/pkulab/view/test/TestActivity.java
@@ -99,7 +99,7 @@ public class TestActivity extends BaseActivity<TestActivityView, TestActivityPre
         ButterKnife.bind(this);
 
         screenPagerIndicator.setDynamicCount(false);
-        screenPagerIndicator.setCount(TestScreens.values().length - 1); // Cancel screen is ignored
+        screenPagerIndicator.setCount(TestScreens.showDotFor().size());
     }
 
     @Override
@@ -233,7 +233,7 @@ public class TestActivity extends BaseActivity<TestActivityView, TestActivityPre
         }
 
         showFragment(fragment, addToBackStack, withAnimation);
-        screenPagerIndicator.setSelection(screen.ordinal());
+        screenPagerIndicator.setSelection(Math.min(screen.ordinal(), TestScreens.showDotFor().size()));
         presenter.checkBattery();
     }
 
@@ -289,7 +289,7 @@ public class TestActivity extends BaseActivity<TestActivityView, TestActivityPre
     @Override
     public void showPreviousScreen() {
         onBackPressedHere();
-        screenPagerIndicator.setSelection(getCurrentScreen().ordinal());
+        screenPagerIndicator.setSelection(Math.min(getCurrentScreen().ordinal(), TestScreens.showDotFor().size()));
     }
 
     @Override
