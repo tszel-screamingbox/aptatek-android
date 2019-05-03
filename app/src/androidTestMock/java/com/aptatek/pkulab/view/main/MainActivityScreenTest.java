1ab37ce788319d0dbed67a07083fbbaadcf2abf0
Tamas Szelezsan <tamas.szelezsan@screamingbox.com>
2019-05-03 14:40:44 +0200

    Fix rebase

--- app/src/androidTestMock/java/com/aptatek/pkulab/view/main/MainActivityScreenTest.java
+++ app/src/androidTestMock/java/com/aptatek/pkulab/view/main/MainActivityScreenTest.java
@@ -11,8 +11,8 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
